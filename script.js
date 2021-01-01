57fe7d6a6e1bde35f3ae0e5b510757a57134dc29
EJostad <eric.jostad@outlook.com>
2020-12-31 21:44:14 -0800

    Added functionality where the relevant weather data is called via API directly from the users input.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
 document.getElementById("searchCity").addEventListener("click", function searchCity() {
     console.log("click!")
 
-    var userInput = "portland";
+    var userInput = document.getElementById("userInput").value;;
 
     var city = userInput; 
 
